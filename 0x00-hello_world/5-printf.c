2f377db88c5bee99c533f834abc7a2c0a3aae610
Zainab Salih <zainab.salih@outlook.com>
2023-09-07 06:10:15 -0700

    modified c programs

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ *main - display "with proper grammer, but the outcome is a piece of art"
+ *Return: 0 and exit the program
+ */
+
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return(0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+ 
+return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 
+/**
+ *main - display the size of various data types on the computer
+ *Return: 0 and exit the program
+ */
+
 int main(void)
 {
-  int i;
-  long int l;
-  long long int k;
-  float f;
-  char c;
+int i;
+long int l;
+long long int k;
+float f;
+char c;
 
-  printf("Size of int: %lu\n", (unsigned long)sizeof(i));
-  printf("Size of long int: %lu\n", (unsigned long)sizeof(l));
-  printf("Size of long long int: %lu\n", (unsigned long)sizeof(k));
-  printf("Size of float: %lu\n", (unsigned long)sizeof(f));
-  printf("Size of char: %lu\n", (unsigned long)sizeof(c));
+printf("Size of int: %lu\n", (unsigned long)sizeof(i));
+printf("Size of long int: %lu\n", (unsigned long)sizeof(l));
+printf("Size of long long int: %lu\n", (unsigned long)sizeof(k));
+printf("Size of float: %lu\n", (unsigned long)sizeof(f));
+printf("Size of char: %lu\n", (unsigned long)sizeof(c));
 
-  return(0);
+return (0);
 }
