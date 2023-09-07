770241a5461f820dcf6a36ffe8990a4b5bc3e28a
Zainab Salih <zainab.salih@outlook.com>
2023-09-07 06:21:37 -0700

    modified c programs

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ long long int k;
 float f;
 char c;
 
-printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(i));
-printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(l));
-printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(k));
-printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
-printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(k));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 
 return (0);
 }
