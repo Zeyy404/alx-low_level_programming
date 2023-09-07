fd3f0559b906a44b035eb6ecd93bf4d8f8598479
Zainab Salih <zainab.salih@outlook.com>
2023-09-07 05:45:46 -0700

    modified c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main -display "Programming is like building a multilingual puzzle"
+ * Return: 0 and exit the program 
+ */
 int main(void)
 {
   const char *message = "Programming is like building a multilingual puzzle\n";
   puts(message);
-  return(0);
+  return (0);
 }
