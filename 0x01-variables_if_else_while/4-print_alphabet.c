f9aa618efa218ec30b7cfb5375337ac7e8748d2c
Zainab Salih <zainab.salih@outlook.com>
2023-09-08 15:32:44 -0700

    added new c program

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lowercase except for q and e
+ * Return: 0 and exit the program
+ */
+int main(void)
+{
+	char c;
+
+	c = 97;
+	while (c < 123 && c != 102 || c != 114)
+		putchar((char)c), c++;
+	putchar('\n');
+	return (0);
+}
