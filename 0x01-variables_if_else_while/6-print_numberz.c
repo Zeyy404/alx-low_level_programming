a5a10d37944ebe7c3741aeacee1dc9731551de9a
Zainab Salih <zainab.salih@outlook.com>
2023-09-08 16:19:43 -0700

    added new c program

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ * Return: 0 and exit the program
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+		putchar((int)n);
+	putchar('\n');
+	return (0);
+}
