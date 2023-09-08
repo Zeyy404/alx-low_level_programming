f59696b9aeaf9dbf1cd233c84f89f13a0e52d201
Zainab Salih <zainab.salih@outlook.com>
2023-09-08 15:18:12 -0700

    added new c program

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,21 +2,18 @@
 
 /**
  * main - prints the alphabet in lowercase, and then in uppercase
- * Return: 0 and exit the program 
+ * Return: 0 and exit the program
  */
 int main(void)
 {
 	char c;
 
-	c = 123;
-	while (c > 64)
-		{
-			if (c > 96)
-				putchar((char)c);
-			else if (c < 90 && c > 64)
-				putchar((char)c);
-			c--;
-		}
+	c = 97;
+	while (c < 123)
+		putchar((char)c), c++;
+	c = c /2 + 4;
+	while (c < 91)
+		putchar((char)c), c++;
 	putchar('\n');
 	return (0);
 }
