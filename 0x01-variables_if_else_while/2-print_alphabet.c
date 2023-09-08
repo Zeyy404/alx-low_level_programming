b87e600efccb2c924d9d957036995f87ec833837
Zainab Salih <zainab.salih@outlook.com>
2023-09-08 08:23:12 -0700

    added a new c program

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lowercse
+ * Return: 0 and exit the program
+ */
+int main(void)
+{
+	int num;
+	num = 97;
+
+	while (num < 123)
+		putchar((char)num);
+	putchar("\n");
+	
+	return (0);
+}
