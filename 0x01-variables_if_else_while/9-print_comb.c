2a7068e288e63c07359d0970812da8516f1e99cf
Zainab Salih <zainab.salih@outlook.com>
2023-09-08 17:04:06 -0700

    added new c program

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combination of single-digit numbers
+ * Return: 0 and exit the program
+ */
+int main(void)
+{
+	int n;
+
+	n = 48;
+	while (n < 58)
+		{
+			putchar((char)n);
+			putchar(", ");
+			n++;
+		}
+	putchar('\n');
+	return (0);
+}
