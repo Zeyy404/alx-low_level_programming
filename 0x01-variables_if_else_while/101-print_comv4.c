8a45789e52dfbbf13bd7ed419d1f228086884985
Zainab Salih <zainab.salih@outlook.com>
2023-09-11 07:03:28 -0700

    added a C program

---
+++ 0x01-variables_if_else_while/101-print_comv4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - prints all different combinations of three digits, no repetition
+ * Return: 0 and exit the program
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	i = 48;
+	while (i < 56)
+	{
+		j = i;
+		while (j < 57)
+		{
+			k = j;
+			while (k < 58)
+			{
+				if (k != j)
+				{
+					putchar((char)i);
+					putchar((char)j);
+					putchar((char)k);
+				}
+				if (i != 55)
+				{
+					putchar(44);
+					putchar(32);
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
