eb391abe3bc1d97377835fb1929d3e663464861a
Zainab Salih <zainab.salih@outlook.com>
2023-09-11 04:45:50 -0700

    added a C program

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible different combination of two digits
+ * Return: 0 and exit the program
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	i = 48;
+	j = 49;
+	while (i < 57 && j < 58)
+	  {
+		putchar((char)i);
+		putchar((char)j);
+		putchar(44);
+		putchar(32);
+	  }
+	putchar('\n');
+  	return (0);
+}
