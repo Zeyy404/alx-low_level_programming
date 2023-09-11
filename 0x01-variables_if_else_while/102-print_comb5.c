28d23dc44f152ee64a8fd969bc90def679560a61
Zainab Salih <zainab.salih@outlook.com>
2023-09-11 13:55:12 -0700

    modified C program

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,46 +1,30 @@
 #include <stdio.h>
 
-/**
- * main - prints all combinations of two two-digit number, no repetition
- * Return: 0 and exist program
- */
 int main(void)
 {
 	int i;
 	int j;
-	int k;
-	int l;
 
-	i = 48;
-	while (i < 58)
+	i = 0;
+	while (i <= 99)
 	{
-		j = 48;
-		while (j < 58)
+		j = i;
+		while (j <= 99)
 		{
-			k = i;
-			while (k < 58)
+			putchar((i / 10) + 48);
+			putchar((i % 10) + 48);
+			putchar(32);
+			putchar((j / 10) + 48);
+			putchar((j % 10) + 48);
+			if (i != 99 || j != 99)
 			{
-				l = j + 1;
-				while (l < 58)
-				{
-					putchar((char)i);
-					putchar((char)j);
-					putchar(32);
-					putchar((char)k);
-					putchar((char)l);
-					l++;
-					if (l > j)
-					{
-						putchar(44);
-						putchar(32);
-					}
-				}
-				k++;
+				putchar(44);
+				putchar(32);
 			}
 			j++;
 		}
 		i++;
 	}
 	putchar('\n');
-	return (0);
+	return 0;
 }
