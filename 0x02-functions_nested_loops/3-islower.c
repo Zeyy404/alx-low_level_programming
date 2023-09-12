7d741cc1a1872937e8d1fd1cbd862dba22aee91c
Zainab Salih <zainab.salih@outlook.com>
2023-09-12 22:38:57 +0000

    added _islower function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * Return: 1 if c is lowercase & 0 otherwise
+ */
+int _islower(int c)
+{
+	char c;
+
+	if (c > 96 && c < 123)
+		_putchar(48);
+	else
+		_putchar(49);
+	_putchar('\n');
+	return (0);
+}
