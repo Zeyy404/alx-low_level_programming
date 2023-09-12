9ad0ef020723e9f8132288b35e02a83f27016327
Zainab Salih <zainab.salih@outlook.com>
2023-09-12 23:40:44 +0000

    added print_last_digit function

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: the number
+ * Return: the last digit of the number
+ */
+int print_last_digit(int n)
+{
+	n = n % 10;
+	return (n);
+}
