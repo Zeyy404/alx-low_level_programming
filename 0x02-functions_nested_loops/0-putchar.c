f4dbbae39e207307b0e161beffdf49eeb143d1ce
Zainab Salih <zainab.salih@outlook.com>
2023-09-12 21:27:25 +0000

    st up project directory and added a C progarm

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar, followed by a new line
+ * Return: 0 and exit the program
+ */
+int main(void)
+{
+	char c[];
+	int i;
+
+	c[] = "_putchar";
+	i = 0;
+	while (c[i] < '\0')
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	return (0);
+}
