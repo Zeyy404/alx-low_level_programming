4283bb19834ecf471c80435587ed91d0d028f5d9
Zainab Salih <zainab.salih@outlook.com>
2023-09-12 23:37:09 +0000

    added _abs function

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,14 +2,13 @@
 
 /**
  * _abs - computes the absolute value of an integer
+ * @i: the integer
  * Return: absolute value
  */
-int _abs(int)
+int _abs(int i)
 {
-	int n;
-
-	if (n => 0)
-		return (n);
+	if (i > 0 || i == 0)
+		return (i);
 	else
-		return (-n);
+		return (-i);
 }
