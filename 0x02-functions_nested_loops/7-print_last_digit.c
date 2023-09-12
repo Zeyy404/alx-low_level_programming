4e05b9e988a82e93ee7f159c1f2b428c8598dc65
Zainab Salih <zainab.salih@outlook.com>
2023-09-12 23:44:02 +0000

    added print_last_digit function

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,10 +3,11 @@
 /**
  * print_last_digit - prints the last digit of a number
  * @n: the number
- * Return: the last digit of the number
+ * Return: the value of the last digit
  */
 int print_last_digit(int n)
 {
 	n = n % 10;
+	_putchar(n);
 	return (n);
 }
