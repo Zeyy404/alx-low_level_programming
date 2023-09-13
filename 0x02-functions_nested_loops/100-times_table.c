f8eb485adcd41fed00d0afe21b5518b22e34a767
Zainab Salih <zainab.salih@outlook.com>
2023-09-13 17:31:50 +0000

    added print_times_table function

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,7 @@ void print_times_table(int n)
 	int i;
 	int j;
 
-	if (n < 15 && n > 0)
+	if (n < 15 && n > -1)
 	{
 		for (i = 0 ; i < n + 1 ; i++)
 		{
@@ -22,27 +22,27 @@ void print_times_table(int n)
 				}
 				else if (i * j < 10)
 				{
-					_putchar(44);
-					_putchar(32);
 					_putchar(32);
 					_putchar(32);
 					_putchar((i * j) + 48);
 				}
 				else if (i * j > 9 && i * j < 100)
 				{
-					_putchar(44);
-					_putchar(32);
 					_putchar(32);
 					_putchar(((i * j) / 10) + 48);
 					_putchar(((i * j) % 10) + 48);
 				}
 				else if (i * j > 99)
 				{
+					_putchar(((i * j) + 48) / 100);
+					_putchar((((i * j) + 48) / 10) % 10);
+					_putchar(((i * j) + 48) % 10);
+				}
+
+				if (i != n)
+				{
 					_putchar(44);
 					_putchar(32);
-					_putchar(((i * j) + 48) / 10);
-					_putchar(((i * j) + 48) % 10);
-					_putchar((((i * j) + 48) / 10) % 10);
 				}
 			}
 			_putchar('\n');
