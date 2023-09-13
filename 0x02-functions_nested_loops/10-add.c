5b3368539d0269643057a12ec3d90cb5ca302e83
Zainab Salih <zainab.salih@outlook.com>
2023-09-13 02:15:32 +0000

    added add function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @i & @j: the integrs
+ * Return: the additional result
+ */
+int add(int i, int j)
+{
+	int n;
+
+	n = i + j;
+	return (n);
+}
