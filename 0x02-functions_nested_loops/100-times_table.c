05d13a56e1602fcc78a8b97c3fee25c583e2a6d9
Zainab Salih <zainab.salih@outlook.com>
2023-09-13 16:56:19 +0000

    added print_times_table function

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times of table, starting from 0
+ * @n: the number of times to print the table
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	while (n < 15 && n > 0)
+	{
+		for (int i = 0 ; i < n + 1 ; i++)
+		{
+			for (int j = 0 ; j < n + 1 ; j++)
+			{
+				if (j == 0)
+				{
+					_putchar((i * j) + 48);
+				}
+				else if (i * j < 10)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+					_putchar((i * j) + 48);
+				}
+				else
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(((i * j) / 10) + 48);
+					_putchar(((i * j) % 10) + 48);
+				}
+			}
+		}
+	}
+}
