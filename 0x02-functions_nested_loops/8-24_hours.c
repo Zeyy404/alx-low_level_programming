d733dfea6690a6e8d62f8b30c1fe5f8b97df604a
Zainab Salih <zainab.salih@outlook.com>
2023-09-13 00:08:15 +0000

    added jack_bauer function

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,10 +17,11 @@ void jack_bauer(void)
 		{
 			_putchar((i / 10) + 48);
 			_putchar((i % 10) + 48);
+			_putchar(58);
 			_putchar((j / 10) + 48);
 			_putchar((j % 10) + 48);
 			j++;
 		}
-		i++
+		i++;
 	}
 }
