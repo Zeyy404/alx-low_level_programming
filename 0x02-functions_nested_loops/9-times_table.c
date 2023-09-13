2cc0cddd090a3118937a2ddae9c18f6cf959dfdd
Zainab Salih <zainab.salih@outlook.com>
2023-09-13 01:03:51 +0000

    added times_table function

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,7 +25,7 @@ void times_table(void)
 			if ((i != 9 || j != 9) && j != 9)
 			{
 				_putchar(44);
-				if (i * j < 10 && i < 2 && j < 2)
+				if (i * j < 10 && (i < 5 || j < 3))
 				{
 					_putchar(32);
 					_putchar(32);
