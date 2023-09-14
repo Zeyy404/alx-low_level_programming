10fe39548bc395a473398f0be45bcc0dea52d9af
Zainab Salih <zainab.salih@outlook.com>
2023-09-14 17:35:20 +0000

    added print_triangle function

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,20 +7,19 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, k;
 
 	if (size > 0)
 	{
 		for (i = 0 ; i < size ; i++)
 		{
-			for (j = 0 ; j < size ; j++)
+			for (j = size ; j > i + 1 ; j--)
 			{
-				if (j == i)
-					_putchar(35);
-				else
-				{
-					_putchar(32);
-				}
+				_putchar(32);
+			}
+			for (k = 0 ; k < i + 1 ; k++)
+			{
+				_putchar(35);
 			}
 			_putchar('\n');
 		}
