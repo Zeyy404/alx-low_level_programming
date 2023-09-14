ecfc6235de49c13b7e13a1d1981de157f6ba7e8e
Zainab Salih <zainab.salih@outlook.com>
2023-09-14 17:04:49 +0000

    added print_triangle function

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a traingle
+ * @size: the size of the triangle
+ * Return: void
+ */
+void print_traingle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0 ; i < size ; i++)
+		{
+			for (j = 0 ; j < size ; j++)
+			{
+				if (j == i)
+					_putchar(35);
+				else
+				{
+					_putchar(32);
+				}
+			}
+			_putchar('\n');
+		}
+
+	}
+	else
+		_putchar('\n');
+}
