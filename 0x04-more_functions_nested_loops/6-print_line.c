b599a6d67b82a1785fe7209aadcd93ec6e75c92c
Zainab Salih <zainab.salih@outlook.com>
2023-09-14 14:24:22 +0000

    added print_line function

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: the number of times the character _ should be printed
+ * Return: void
+ */
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
