9c0dea58cb71046ffb99465339e423ac1a48922d
Zainab Salih <zainab.salih@outlook.com>
2023-09-14 11:59:39 +0000

    added print_numbers function

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i, n;
+
+	n = 48;
+	for (i = 0 ; i < 58 ; i++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
