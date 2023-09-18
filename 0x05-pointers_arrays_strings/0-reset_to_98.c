d13375b426067f20e858f84b5c648284fe7be655
Zainab Salih <zainab.salih@outlook.com>
2023-09-18 17:11:30 +0000

    Added reset_to_98 function

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,26 +2,17 @@
 
 /**
  * reset_to_98 - takes a pointer to an int and update its value
- * @*n: the pointer
+ * @n: the pointer
  * Return: void
  */
 void reset_to_98(int *n)
 {
-	int integer, i;
-	char prt[] = "n=";
+	int integer;
 
-	for (i = 0 ; i !='\0' ; i++)
-	{
-		_putchar(prt[i]);
-	}
 	_putchar(integer + 48);
 	_putchar('\n');
 	n = &integer;
 	*n = 98;
-	for (i = 0; i !='\0' ; i++)
-	{
-		_putchar(prt[i]);
-	}
 	_putchar(integer + 48);
 	_putchar('\n');
 }
