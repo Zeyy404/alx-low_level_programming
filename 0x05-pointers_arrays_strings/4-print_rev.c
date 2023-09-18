f915b1240087687eda80fa6ea0c1ff404ce9690e
Zainab Salih <zainab.salih@outlook.com>
2023-09-18 19:12:40 +0000

    Added print_rev function

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: Type char pointer
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = '\0';
+	while (s[i] > -1)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
