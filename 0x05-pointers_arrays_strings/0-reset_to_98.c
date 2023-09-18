affab46dd7a42f2739c63c0799dcac97c6599c61
Zainab Salih <zainab.salih@outlook.com>
2023-09-18 17:26:15 +0000

    Added reset_to_98 function

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -11,13 +11,21 @@ void reset_to_98(int *n)
 	char prt[] = "n=";
 
 	for (i = 0 ; i != '\0' ; i++)
+	{
 		_putchar(prt[i]);
-	_putchar(integer + 48);
+		while (integer != 0)
+		{
+			_putchar((integer % 10) + 48);
+			integer =/ 10;
+		}
+	}
+
 	_putchar('\n');
 	for (i = 0 ; i != '\0' ; i++)
 		_putchar(prt[i]);
 	n = &integer;
 	*n = 98;
-	_putchar(integer + 48);
+	_putchar((integer / 10) + 48);
+	_putchar((integer % 10) + 48);
 	_putchar('\n');
 }
