fff7a093846c5b78a983ce52628e9f62522d2d3c
Zainab Salih <zainab.salih@outlook.com>
2023-09-18 18:30:43 +0000

    Added _strlen function

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * strlen - calculates the length of a string
+ * _strlen - calculates the length of a string
  * @s: Type char pointer
  * Return: the length
  */
 int _strlen(char *s)
 {
-	int i = 0;
-
-	do
+	int i;
+	
+	for (i = 0 ; *s != '\0'; i++)
 	{
-		i++;
-	} while (*s != '\0');
-
+		*s++;
+	}
 	return (i);
 }
