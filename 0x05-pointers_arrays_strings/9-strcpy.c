f30427ff75f78447b827ffb2f50b5c44f769e211
Zainab Salih <zainab.salih@outlook.com>
2023-09-19 12:47:22 +0000

    Added _strcpy function

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcpy - cpoies the string pointed by src to the buffer pointed to by dest
+ * @src: type char pointer
+ * @dest: type char pointer
+ * Return: dest
+ */
+char _strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0 ; src[i] != '\0' ; i++)
+	{
+		dest[i] = src[i];
+		return (dest[i]);
+	}
+}
