772ba95d662d77ac1c1ba9aaa113a479a8ad4a75
Zainab Salih <zainab.salih@outlook.com>
2023-09-19 12:34:21 +0000

    Added print_array function

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,8 +13,11 @@ void print_array(int *a, int n)
 
 	for (i = 0 ; i < n ; i++)
 	{
-		printf("%d, ", a[n]);
+		if (i != (n - 1))
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i])
 	}
 
-	_putchar('\n');
+	printf("\n");
 }
