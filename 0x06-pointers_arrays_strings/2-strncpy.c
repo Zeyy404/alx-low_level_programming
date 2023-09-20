bc7a5fec957d02185a232ad01772ace666055494
Zainab Salih <zainab.salih@outlook.com>
2023-09-20 19:54:43 +0000

    Added _strncpy function

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,15 +11,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0 ; i <= n ; i++)
+	for (i = 0 ; str[i] != '\0' && i < n ; i++)
 	{
 		dest[i] = src[i];
-
-		while (src[i] == '\0' && i <= n)
-		{
-			dest[i] = '\0';
-			i++;
-		}
 	}
+	for (; i < n ; i++)
+		dest[i] = '\0';
+
 	return (dest);
 }
