5d9ca99f756fddd0c874dccb45fe291263bae104
Zainab Salih <zainab.salih@outlook.com>
2023-09-20 18:34:25 +0000

    Added _strcat function

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: type char pointer
+ * @src: type char pointer
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	i++;
+	while (src[j] != '\0')
+	{
+		dest[i] = _putchar(src[j]);
+		j++;
+		i++;
+	}
+	i++;
+	dest[i] = '\0';
+	return (dest);
+}
