1eeab88f2a4ca558a303265f45afc6045369aee8
Zainab Salih <zainab.salih@outlook.com>
2023-09-20 01:00:17 +0000

    Added _atoi function

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -19,10 +19,10 @@ int _atoi(char *s)
 		}
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			result = result * -1 * 10 + (s[i] - '0');
+			result = result * 10 + (s[i] - '0');
 		}
 		i++;
 	}
-	sign = sign * -1;
+
 	return (result * sign);
 }
