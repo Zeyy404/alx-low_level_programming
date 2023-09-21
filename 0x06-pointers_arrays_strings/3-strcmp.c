b80594724f623840e417ffcc5fa7579ec16839c3
Zainab Salih <zainab.salih@outlook.com>
2023-09-21 11:23:13 +0000

    Added _strcmp function

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,11 +13,9 @@ int _strcmp(char *s1, char *s2)
 	result = 0;
 	for (i = 0 ; (s1[i] != '\0') && (s2[i] != '\0') ; i++)
 	{
-		if (s1[i] == s2[i])
-			;
-		else
+		if (s1[i] != s2[i])
 		{
-			result = (s1[i] - s2[i]) + 48;
+			result = s1[i] - s2[i];
 			break;
 		}
 	}
