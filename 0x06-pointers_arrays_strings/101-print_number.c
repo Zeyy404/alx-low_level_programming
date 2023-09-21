e670394fe9e8db95bb5dc39e00a67abf2446f944
Zainab Salih <zainab.salih@outlook.com>
2023-09-21 17:55:24 +0000

    Added print_number function

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: int variable
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar(45);
+		n = n * -1;
+	}
+
+	if (n / 10 > 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + 48);
+}
