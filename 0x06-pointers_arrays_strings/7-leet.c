626756529c148c3a53af77e4bdbadd580968c1ff
Zainab Salih <zainab.salih@outlook.com>
2023-09-21 14:51:44 +0000

    Added leet function

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * leet - ecodes a string into 1337
+ * @str: type char pointer
+ * Return: str
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char letter[] = "aAeEoOtTlL";
+	char num[] = "4433007711";
+
+	for (i = 0; str[i] != '\0' ; i++)
+	{
+		for (j = 0; letter[j] != '\0' ; j++)
+		{
+			if (str[i] == letter[j])
+				str[i] = num[j];
+		}
+	}
+	return (str);
+}
