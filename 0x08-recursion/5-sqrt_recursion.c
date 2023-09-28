e53e7d3480e9329856c7e7b643e8beb66bf6fae6
Zainab Salih <zainab.salih@outlook.com>
2023-09-28 22:11:41 +0000

    Added _sqrt_recursion function

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _sqrt_rec - finds the natural sqr root
+ * @n: input number
+ * @x: int
+ * Return: sqr root value
+ */
+int _sqrt_rec(int x, int y)
+{
+	if (y * y == x)
+		return (y);
+	else if (y * y < x)
+		return (_sqrt_rec(x, y + 1));
+	else
+		return (-1);
+}
+
+/**
+ * _sqrt_recursion - prints the natural square root of a number
+ * @n: input number
+ * Return: if n doesn't have a nat sqr root -1, else value of sqr root
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt_rec(n, 1));
+}
