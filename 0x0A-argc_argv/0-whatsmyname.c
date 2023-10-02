6e915d342a64a0c6b625f2025d031ded29b6a985
Zainab Salih <zainab.salih@outlook.com>
2023-10-02 18:22:47 +0000

    Added mynameis Program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <main.h>
+
+/**
+ * main - prints the name of the program, followed by new line
+ * @argc: argument count
+ * @argv: string vector
+ * Return: 0 and exit
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
