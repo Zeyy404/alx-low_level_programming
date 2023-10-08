564c3c3ff3b49e2f439b7530c545022237654f3d
Zainab Salih <zainab.salih@outlook.com>
2023-10-08 19:00:24 +0000

    Added _realloc function

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,27 +14,29 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *_ptr = NULL;
 	unsigned int i, cbytes;
 
-	if (new_size > old_size || ptr == NULL)
-	{
-		_ptr = malloc(new_size);
-		if (_ptr == NULL)
-			return (NULL);
-
-		cbytes = (old_size < new_size) ? old_size : new_size;
-		for (i = 0 ; i < cbytes ; i++)
-		{
-			_ptr[i] = ((char *)ptr)[i];
-		}
-
-		free(ptr);
-		return (_ptr);
-	}
 	if (new_size == old_size)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		_ptr = malloc(new_size);
+		if (_ptr == NULL)
+			return (NULL);
+		return (_ptr);
+	}
+
+	_ptr = malloc(new_size);
+	if (_ptr == NULL)
+		return (NULL);
+
+	cbytes = (old_size < new_size) ? old_size : new_size;
+	for (i = 0 ; i < cbytes ; i++)
+	{
+		_ptr[i] = ((char *)ptr)[i];
 	}
 
 	free(ptr);
