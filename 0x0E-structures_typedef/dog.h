51aaa64780a4540a93193aec01f844ed37bc8eba
Zainab Salih <zainab.salih@outlook.com>
2023-10-10 16:53:51 +0000

    Modified dog.h and init_dog files

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - define a new type struct dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
 struct dog
 {
 	char *name;
