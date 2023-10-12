25620a48f1c39afa27ed18a5d8827a8ec1ac45ed
Zainab Salih <zainab.salih@outlook.com>
2023-10-12 22:15:56 +0000

    Added print_all function

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,84 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * handle_char - prints a character
+ * @args: argument to be printed
+ * Return: void
+ */
+void handle_char(va_list args)
+{
+	int value = va_arg(args, int);
+	printf("%c", (char)value);
+}
+
+/**
+ * handle_int - prints an integer
+ * @args: argument to be printed
+ * Return: void
+ */
+void handle_int(va_list args)
+{
+	int value = va_arg(args, int);
+	printf("%d", value);
+}
+/**
+ * handle_float - print a float
+ * @args: argument to be printed
+ * Return: void
+ */
+void handle_float(va_list args)
+{
+	double value = va_arg(args, double);
+	printf("%f", value);
+}
+/**
+ * handle_string - prints a string
+ * @args: argument to be printed
+ * Return: void
+ */
+void handle_string(va_list args)
+{
+	const char *str = va_arg(args, const char *);
+	if (str != NULL)
+		printf("%s", str);
+	else
+		printf("(nil)");
+}
+/**
+ * print_all - prints anything
+ * @format: the type of arguments passed to the function
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	print_t func[] = {
+		{"c", handle_char},
+		{"i", handle_int},
+		{"f", handle_float},
+		{"s", handle_string}
+	};
+	va_list args;
+	int i, j;
+	char *sep = ", ";
+
+	va_start(args, format);
+	i = 0;
+	while (*format && (*(format + i)))
+	{
+		j = 0;
+		while (j < 4 && *(format + i) != *(func[j].specifier))
+			j++;
+
+		if (j < 4)
+		{
+			func[j].action(args);
+			if (j != 3)
+				printf("%s", sep);
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,23 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
+/**
+ * struct func - Struct func
+ *
+ * @specifier: The data type specifier
+ * @action: The function associated
+ */
+typedef struct print
+{
+	char *specifier;
+	void (*action)(va_list args);
+} print_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
