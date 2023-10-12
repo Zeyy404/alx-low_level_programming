5ab267da681c338313fd95a8c9ba4764636ec2f0
Zainab Salih <zainab.salih@outlook.com>
2023-10-12 18:38:34 +0000

    Added sum_them_all function

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,10 +9,9 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i, x, sum = 0;
-
 	va_list args;
-	va_start(args, n);
 
+	va_start(args, n);
 	if (n == 0)
 		return (0);
 	for (i = 0 ; i < n ; i++)
