275f2ec6226ade7ce9fe2d66412a7b5c2621481d
Zainab Salih <Zainab.Salih@outlook.com>
2023-10-24 05:46:07 +0000

    [TASK 9] insert_nodeint function

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,16 +11,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *h;
 	listint_t *new_node;
-	unsigned int i = 1, count = 0;
+	unsigned int i = 1, count;
 
 	if (head == NULL || *head == NULL)
 		return (NULL);
 	h = *head;
-	while (h != NULL)
-	{
-		count++;
-		h = h->next;
-	}
+	count = listint_len(*head);
 	if (idx > count)
 		return (NULL);
 
@@ -29,13 +25,42 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new_node->n = n;
 	h = *head;
-	while (i < idx)
+	while (h != NULL)
 	{
+		if (i == idx)
+		{
+			if (i == 0)
+			{
+				new_node->next = h;
+				h = new_node;
+				break;
+			}
+			new_node->next = h->next;
+			h->next = new_node;
+			break;
+		}
 		h = h->next;
 		i++;
 	}
-	new_node->next = h->next;
-	h->next = new_node;
+	if (i != idx)
+		return (NULL);
+	else
+		return (h->next);
+}
 
-	return (h);
+/**
+ * listint_len - returns the number of elements in a linked listint_t list
+ * @h: a pointer to a linked list
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
 }
