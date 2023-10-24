c226f2cc43d621b4ed035191e708599bef82c910
Zainab Salih <Zainab.Salih@outlook.com>
2023-10-24 06:18:23 +0000

    [TASK 9] insert_nodeint function

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (head == NULL || *head == NULL)
 		return (NULL);
-	h = *head;
+
 	count = listint_len(*head);
 	if (idx > count)
 		return (NULL);
@@ -24,25 +24,25 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 	h = *head;
 	while (h != NULL)
 	{
 		if (i == idx)
 		{
-			if (i == 0)
-			{
-				new_node->next = h;
-				h = new_node;
-				break;
-			}
 			new_node->next = h->next;
 			h->next = new_node;
 			break;
 		}
-		h = h->next;
 		i++;
+		h = h->next;
 	}
-	return (h->next);
+	return (new_node);
 }
 
 /**
