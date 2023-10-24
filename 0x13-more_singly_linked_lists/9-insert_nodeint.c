df67aed5262504b5addae29bf63c87f2dba0fb96
Zainab Salih <Zainab.Salih@outlook.com>
2023-10-24 05:49:11 +0000

    [TASK 9] insert_nodeint function

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -42,10 +42,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		h = h->next;
 		i++;
 	}
-	if (i != idx)
-		return (NULL);
-	else
-		return (h->next);
+	return (h->next);
 }
 
 /**
