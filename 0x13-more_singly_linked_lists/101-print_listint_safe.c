30195f459a5a3de8aad3914006e73652376b7324
Zainab Salih <Zainab.Salih@outlook.com>
2023-10-25 00:17:15 +0000

    [TASK 12] print_listint_safe function

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a listint_t linked list
+ * @head: a pointer to a linked list
+ * Return: the number of nodes
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *h, *_h;
+	size_t i;
+
+	if (head == NULL)
+		return (98);
+
+	h = _h = head;
+	i = 0;
+	while (h != NULL)
+	{
+		printf("[%p] %d\n", (void *)h, h->n);
+		i++;
+		h = h->next;
+		_h = _h->next->next;
+		if (h == _h && h->next == _h->next)
+		{
+			h = h->next;
+			printf("-> [%p] %d\n",(void *)h, h->n);
+			return (i);
+		}
+	}
+	return (i);
+}
---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds the loop in a linked list
+ * @head: a pointer to the head of a linked list
+ * Rreturn: the address of the node where the loop starts, or NULL.
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *h, *_h;
+
+	h = _h = head;
+	while (_h != NULL && _h->next != NULL)
+	{
+		h = h->next;
+		_h = _h->next->next;
+
+		if (h == _h)
+		{
+			return (h->next);
+		}
+	}
+	return (NULL);
+}
