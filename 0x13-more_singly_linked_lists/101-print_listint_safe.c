65f7f0c5a7ab90c250f3e09c8922529852569429
Zainab Salih <Zainab.Salih@outlook.com>
2023-10-25 01:42:03 +0000

    [TASK 12] print_listint_safe function

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,6 +9,7 @@ size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *h, *_h;
 	size_t i;
+	int loop = 0;
 
 	if (head == NULL)
 		return (98);
@@ -17,21 +18,32 @@ size_t print_listint_safe(const listint_t *head)
 	i = 0;
 	while (_h != NULL && _h->next != NULL)
 	{
-		printf("[%p] %d\n", (void *)h, h->n);
-		i++;
 		h = h->next;
 		_h = _h->next->next;
-
 		if (h == _h)
 		{
+			loop = 1;
 			h = head;
-			while (h != _h)
+			while (h != _h->next)
 			{
+				printf("[%p] %d\n", (void *)h, h->n);
 				h = h->next;
-				_h = _h->next;
+				i++;
 			}
+			printf("[%p] %d\n", (void *)h, h->n);
+			h = h->next;
+			i++;
 			printf("-> [%p] %d\n", (void *)h, h->n);
 			return (i);
+		}
+	}
+	if (!loop)
+	{
+		h = head;
+		while (h != NULL)
+		{
+			printf("[%p] %d\n", (void *)h, h->n);
+			h = h->next;
 		}
 	}
 	return (i);
