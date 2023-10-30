b1113ae3e14017810cc8d1a612fad710c82df2ed
Zainab Salih <Zainab.Salih@outlook.com>
2023-10-30 09:21:16 +0000

    [TASK 1]

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,9 +17,14 @@ int create_file(const char *filename, char *text_content)
 	if (op == -1)
 		return (-1);
 
-	c = write(op, text_content, strlen(text_content));
-	if (c == -1)
-		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[len] != '\0')
+			len++;
+		c = write(op, text_content, len);
+		if (c != len)
+			return (-1);
+	}
 
 	close(op);
 	return (1);
