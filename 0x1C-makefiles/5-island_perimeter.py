dc684eef1992f76d81ef2f452ac4231613199b73
Zainab Salih <Zainab.Salih@outlook.com>
2024-02-21 19:02:38 +0000

    TASK 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,7 +11,6 @@ def island_perimeter(grid):
                  1 represents a land zone 
     """
     perimeter = 0
-    neighbors = []
 
     for i in range(len(grid)):
         for j in range(len(grid[0])):
