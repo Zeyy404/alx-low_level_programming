20333e1316b187df0f07b81be742dc1aa718abc2
Zainab Salih <Zainab.Salih@outlook.com>
2024-05-08 18:00:57 +0000

    Task 9 - interpolation searching algorithm

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -21,7 +21,7 @@ int interpolation_search(int *array, size_t size, int value)
 		pos = (low + (((double)(high - low) / (array[high] - array[low]))
 			      * (value - array[low])));
 
-		printf("Value checked at array[%ld] = [%d]\n", pos, array[pos]);
+		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
 
 		if (array[pos] == value)
 			return (pos);
